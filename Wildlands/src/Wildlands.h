5767d09dad5bfaee60f15e55c1927e616af4189f
WwEIiC <1073780876@qq.com>
2023-12-31 21:55:51 +0800

    [Add]  -Add spdlog as a submodule. [Change]  - [Fix]  -bug1: file gitignore didn't work.  -reason:it had the wrong name gitingnore.

--- Wildlands/src/Wildlands.h
+++ Wildlands/src/Wildlands.h
@@ -2,6 +2,7 @@
 //This will just be used by applications that use WildlandsEngine.
 
 #include "Wildlands/Application.h"
+#include "Wildlands/Log.h"
 
 //--------------Entry Point-------------------------
 #include "Wildlands/EntryPoint.h"
---
+++ Wildlands/src/Wildlands/Log.h
@@ -0,0 +1,36 @@
+#pragma once
+#include "Core.h"
+#include <memory.h>
+
+#include "spdlog/spdlog.h"
+#include "spdlog/sinks/basic_file_sink.h"
+
+namespace Wildlands
+{
+	class WL_API Log
+	{
+	public:
+		static void Init();
+
+		inline static std::shared_ptr<spdlog::logger>& GetCoreLogger() { return s_CoreLogger; }
+		inline static std::shared_ptr<spdlog::logger>& GetClientLogger() { return s_ClientLogger; }
+
+	private:
+		static std::shared_ptr<spdlog::logger> s_CoreLogger;
+		static std::shared_ptr<spdlog::logger> s_ClientLogger;
+	};
+}
+
+//Core log macros
+#define WL_CORE_TRACE(...)		::Wildlands::Log::GetCoreLogger()->trace(__VA_ARGS__);
+#define WL_CORE_INFO(...)		::Wildlands::Log::GetCoreLogger()->info(__VA_ARGS__);
+#define WL_CORE_WARN(...)		::Wildlands::Log::GetCoreLogger()->warn(__VA_ARGS__);
+#define WL_CORE_ERROR(...)		::Wildlands::Log::GetCoreLogger()->error(__VA_ARGS__);
+#define WL_CORE_FATAL(...)		::Wildlands::Log::GetCoreLogger()->fatal(__VA_ARGS__);
+
+//Client log macros
+#define WL_TRACE(...)			::Wildlands::Log::GetClientLogger()->trace(__VA_ARGS__);
+#define WL_INFO(...)			::Wildlands::Log::GetClientLogger()->info(__VA_ARGS__);
+#define WL_WARN(...)			::Wildlands::Log::GetClientLogger()->warn(__VA_ARGS__);
+#define WL_ERROR(...)			::Wildlands::Log::GetClientLogger()->error(__VA_ARGS__);
+#define WL_FATAL(...)			::Wildlands::Log::GetClientLogger()->fatal(__VA_ARGS__);
